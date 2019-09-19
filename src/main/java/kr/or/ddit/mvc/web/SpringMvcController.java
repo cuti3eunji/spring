c492d691e09abeb18f87356c307c96626cc80862
pej <pejzzango0o@gmail.com>
2019-09-19 16:17:16 +0900

    spring mvc  . ModelAndView  . viewResolver    . beanNameViewResolver (jsonView)    . ajax  . customView   . fileDownloadView

--- src/main/java/kr/or/ddit/mvc/web/SpringMvcController.java
+++ src/main/java/kr/or/ddit/mvc/web/SpringMvcController.java
@@ -1,10 +1,8 @@
 package kr.or.ddit.mvc.web;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
@@ -16,7 +14,6 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +22,9 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import kr.or.ddit.exception.NoFileException;
 import kr.or.ddit.mvc.model.Main;
@@ -40,6 +39,9 @@ import kr.or.ddit.util.model.FileInfo;
 public class SpringMvcController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(SpringMvcController.class);
+	
+	@javax.annotation.Resource(name = "jsonView")
+	private View jsonView;
 	
 	//@RequestMapping이 붙은 메소드가 실행되기 전에 @ModelAttribute 메소드가 먼저 실행되고
 	// 해당 메소드가 리턴하는 값을 Model 객체에 자동으로 넣어준다
@@ -244,5 +246,40 @@ public class SpringMvcController {
 		
 		return "mvc/view";
 	}
+	
+	
+	@RequestMapping("jsonView")
+	public String jsonView(Model model) {
+		List<String> rangers = new ArrayList<String>();
+		rangers.add("brown");
+		rangers.add("cony");
+		rangers.add("sally");
+		
+		model.addAttribute("rangers", rangers);
+		
+		return "jsonView";
+	}
+	
+	
+	@RequestMapping("jsonView2")
+	public View jsonView2(Model model) {
+		List<String> rangers = new ArrayList<String>();
+		rangers.add("brown");
+		rangers.add("cony");
+		rangers.add("sally");
+		
+		model.addAttribute("rangers", rangers);
+		
+		return jsonView;
+		//return new MappingJackson2JsonView();
+	}
+	
+	
+	@RequestMapping("fileDownloadView")
+	public String fileDownloadView(String pictureName, Model model) {
+		model.addAttribute("pictureName", pictureName);
+		return "fileDownloadView";
+	}
+	
 	
 }
